ae73ea3e16094a9f12e18d20d4d2a8c8c8acfff0
siva9598 <sivasubramanian6996@gmail.com>
2021-09-09 09:52:47 +0530

    removed the .git file in frontend folder and added the files

---
+++ projfrontend/src/user/Profile.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+
+const Profile = () => {
+  return (
+    <Base title="Profile page">
+      <h1>Profile page</h1>
+    </Base>
+  );
+};
+export default Profile;
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
+import { signup } from "../auth/helper/index";
+
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    success: false,
+    error: "",
+  });
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch((err) => console.log("error in signup"));
+  };
+
+  const signUpForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Name</label>
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="email"
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                className="form-control"
+                onChange={handleChange("password")}
+                type="password"
+                value={password}
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div
+        className="alert alert-success"
+        style={{ display: success ? "" : "none" }}
+      >
+        New account was created successfully.Please{" "}
+        <Link to="/signin">Login here</Link>
+      </div>
+    );
+  };
+  const errorMessage = () => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Sign up page">
+      {successMessage()}
+      {errorMessage()}
+      {signUpForm()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
+    </Base>
+  );
+};
+
+export default Signup;
